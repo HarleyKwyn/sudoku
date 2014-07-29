0da8e6efbb813f0eb1bd88abb264caf18d898c4e
HarleyKwyn <kwyn.meagher@gmail.com>
2014-07-28 21:43:18 -0700

    Implements grid.js handlebars helper to pre-render board

--- hbs-helpers/grid.js
+++ hbs-helpers/grid.js
@@ -1,6 +1,41 @@
 var _ = require('lodash');
 
 module.exports = function(board){
-  var htmlTable = '<table> \n';
-  return '<div> Imagine this is a table </div>'
+  var htmlTable = "<table data-board="+board+"> \n ";
+  var htmlTableClose = '</table> \n';
+  for(var row = 0; row < 3; row++){
+    htmlTable += "<tr>";
+    for (var column = 0; column < 3; column++) {
+        htmlTable += "<td>" + quadrent([row,column], board) + "</td>";
+    }
+    htmlTable +="</tr>";
+  }
+  htmlTable += htmlTableClose;
+  return htmlTable;
 };
+
+function quadrent(coords, board){
+  var columnMin = coords[0]*3;
+  var columnMax = columnMin + 3;
+  var rowMin = coords[1]*3;
+  var rowMax = rowMin + 3;
+
+  var quadrentHtml = "<table data-quad='"+coords+"'>"
+  
+  for(var i = rowMin; i < rowMax; i++){
+    quadrentHtml += '<tr>'
+    for(var j = columnMin; j < columnMax; j++){
+      var value = board[i*9+j];
+      if(value === 0){
+        quadrentHtml += '<td> <input type="number"></input> </td>'
+      }else{
+        quadrentHtml += '<td class="static">' + value + '</td>' ;  
+      }
+    }
+    quadrentHtml += '</tr>'
+  }
+
+  quadrentHtml += '</table>'
+  
+  return quadrentHtml;
+}
