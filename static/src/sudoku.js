2353795abcfcace52db5819f50acd51d5aad05fe
HarleyKwyn <kwyn.meagher@gmail.com>
2014-07-31 10:23:29 -0700

    Sets up framework for Sudoku object

---
+++ static/src/sudoku.js
@@ -0,0 +1,36 @@
+var Sudoku = function(board){
+  this._board = board;
+  this._origonalBoard = board;
+};
+
+Sudoku.prototype.isValidBoard = function(){
+  this._board
+  return false;
+};
+
+Sudoku.prototype.resetBoard = function(){
+  return false;
+};
+
+Sudoku.prototype.updateBoard=function(index, value){
+  return false;
+};
+
+Sudoku.prototype.createQuadrentSets = function(board){
+  var sets = [];
+  return set;
+};
+
+Sudoku.prototype.createRowSets = function(board){
+  var sets = [];
+  return set;
+};
+
+Sudoku.prototype.createColumnSets = function(board){
+  var sets = [];
+  return sets;
+};
+
+var isValidSet = function(set){
+  return false;
+};
