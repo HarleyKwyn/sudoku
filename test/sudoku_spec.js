56ffb6d8b797f030f54db422c558a30b6ce44457
HarleyKwyn <kwyn.meagher@gmail.com>
2014-08-01 17:00:45 -0700

    Adds tests for get*Set methods

--- test/sudoku_spec.js
+++ test/sudoku_spec.js
@@ -2,8 +2,41 @@ var expect = chai.expect;
 
 describe('Sudoku', function(){
   var validSet = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var invalidSet = [1, 1, 13, 1, 1, 1, 1, 1, 0];
+  var invalidSet = [1, 2, 3, 5, 6, 7, 8, 13, 9];
   var origonalBoard, testboard, validBoard;
+  var origonalRowSets = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+  ] ;
+  var origonalColumnSets = [
+    [5, 6, 0, 8, 4, 7, 0, 0, 0],
+    [3, 0, 9, 0, 0, 0, 6, 0, 0],
+    [0, 0, 8, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 8, 0, 0, 4, 0],
+    [7, 9, 0, 6, 0, 2, 0, 1, 8],
+    [0, 5, 0, 0, 3, 0, 0, 9, 0],
+    [0, 0, 0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 6, 0, 0, 0, 8, 0, 7],
+    [0, 0, 0, 3, 1, 6, 0, 5, 9]
+  ];
+  var origonalQuadrantSets = [
+    [5, 3, 0, 6, 0, 0, 0, 9, 8],
+    [0, 6, 0, 8, 0, 3, 0, 2, 0],
+    [2, 8, 0, 0, 0, 5, 0, 7, 9],
+    [5, 3, 0, 6, 0, 0, 0, 9, 8],
+    [0, 6, 0, 8, 0, 3, 0, 2, 0],
+    [2, 8, 0, 0, 0, 5, 0, 7, 9],
+    [5, 3, 0, 6, 0, 0, 0, 9, 8],
+    [0, 6, 0, 8, 0, 3, 0, 2, 0],
+    [2, 8, 0, 0, 0, 5, 0, 7, 9]  
+  ]
 
   beforeEach(function(done){
     origonalBoard = [ 5, 3, 0, 0, 7, 0, 0, 0, 0, 6, 0, 0, 1, 9, 5, 0, 0, 0, 0, 9, 8, 0, 0, 0, 0, 6, 0, 8, 0, 0, 0, 6, 0, 0, 0, 3, 4, 0, 0, 8, 0, 3, 0, 0, 1, 7, 0, 0, 0, 2, 0, 0, 0, 6, 0, 6, 0, 0, 0, 0, 2, 8, 0, 0, 0, 0, 4, 1, 9, 0, 0, 5, 0, 0, 0, 0, 8, 0, 0, 7, 9 ];
@@ -22,10 +55,23 @@ describe('Sudoku', function(){
     });
   });
 
-  describe('resetBoard', function(){
-    it('should reset board to original state', function(){
-      testboard.resetBoard()
-      expect( testboard._board ).to.equal(origonalBoard);
+  describe('getRowSets', function(){
+    it('should return an array of row sets', function(){
+      expect( testboard.getRowSets() ).to.deep.equal( origonalRowSets );    
+    })
+  });
+
+  describe('getColumnSets', function(){
+    it('should return an array of column sets', function(){
+      expect( testboard.getColumnSets() ).to.deep.equal( origonalColumnSets );    
+    })
+  })
+
+  describe('getQuadrantSets', function(){
+    it('should return array of quadrant sets', function(){
+      console.log(origonalQuadrantSets);
+      console.log(testboard.getQuadrantSets());
+      expect( testboard.getQuadrantSets() ).to.deep.equal( origonalQuadrantSets);
     });
   });
 
